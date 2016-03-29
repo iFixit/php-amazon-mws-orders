254b56d0ab7d6f40a484a51d0cea31217c546602
Chris Opperwall <chriso@ifixit.com>
2016-03-29 09:12:11 -0700

    Remove deprecated iconv_set_encoding uses for php 5.6

--- src/MarketplaceWebServiceOrders/Client.php
+++ src/MarketplaceWebServiceOrders/Client.php
@@ -376,10 +376,6 @@ class MarketplaceWebServiceOrders_Client implements MarketplaceWebServiceOrders_
      */
     public function __construct($awsAccessKeyId, $awsSecretAccessKey, $applicationName, $applicationVersion, $config = null)
     {
-        iconv_set_encoding('output_encoding', 'UTF-8');
-        iconv_set_encoding('input_encoding', 'UTF-8');
-        iconv_set_encoding('internal_encoding', 'UTF-8');
-
         $this->_awsAccessKeyId = $awsAccessKeyId;
         $this->_awsSecretAccessKey = $awsSecretAccessKey;
         if (!is_null($config)) $this->_config = array_merge($this->_config, $config);
